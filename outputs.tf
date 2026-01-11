9f477b2ff6ca609a608d4087287281e80b590b4e
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:46:16 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "api_management_email_templates" {
+  description = "All api_management_email_template resources"
+  value       = azurerm_api_management_email_template.api_management_email_templates
+}
+
