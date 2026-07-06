3b4a6945a9a82b8e3fea1b49c75cfaf2a55c2740
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:10:51 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,32 @@ output "api_management_email_templates" {
   description = "All api_management_email_template resources"
   value       = azurerm_api_management_email_template.api_management_email_templates
 }
+output "api_management_email_templates_api_management_name" {
+  description = "List of api_management_name values across all api_management_email_templates"
+  value       = [for k, v in azurerm_api_management_email_template.api_management_email_templates : v.api_management_name]
+}
+output "api_management_email_templates_body" {
+  description = "List of body values across all api_management_email_templates"
+  value       = [for k, v in azurerm_api_management_email_template.api_management_email_templates : v.body]
+}
+output "api_management_email_templates_description" {
+  description = "List of description values across all api_management_email_templates"
+  value       = [for k, v in azurerm_api_management_email_template.api_management_email_templates : v.description]
+}
+output "api_management_email_templates_resource_group_name" {
+  description = "List of resource_group_name values across all api_management_email_templates"
+  value       = [for k, v in azurerm_api_management_email_template.api_management_email_templates : v.resource_group_name]
+}
+output "api_management_email_templates_subject" {
+  description = "List of subject values across all api_management_email_templates"
+  value       = [for k, v in azurerm_api_management_email_template.api_management_email_templates : v.subject]
+}
+output "api_management_email_templates_template_name" {
+  description = "List of template_name values across all api_management_email_templates"
+  value       = [for k, v in azurerm_api_management_email_template.api_management_email_templates : v.template_name]
+}
+output "api_management_email_templates_title" {
+  description = "List of title values across all api_management_email_templates"
+  value       = [for k, v in azurerm_api_management_email_template.api_management_email_templates : v.title]
+}
 
