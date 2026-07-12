c97bde8da699c1093621485b1362496bbecb521b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:27 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "api_management_email_templates" {
-  description = "All api_management_email_template resources"
-  value       = azurerm_api_management_email_template.api_management_email_templates
-}
 output "api_management_email_templates_api_management_name" {
-  description = "List of api_management_name values across all api_management_email_templates"
-  value       = [for k, v in azurerm_api_management_email_template.api_management_email_templates : v.api_management_name]
+  description = "Map of api_management_name values across all api_management_email_templates, keyed the same as var.api_management_email_templates"
+  value       = { for k, v in azurerm_api_management_email_template.api_management_email_templates : k => v.api_management_name }
 }
 output "api_management_email_templates_body" {
-  description = "List of body values across all api_management_email_templates"
-  value       = [for k, v in azurerm_api_management_email_template.api_management_email_templates : v.body]
+  description = "Map of body values across all api_management_email_templates, keyed the same as var.api_management_email_templates"
+  value       = { for k, v in azurerm_api_management_email_template.api_management_email_templates : k => v.body }
 }
 output "api_management_email_templates_description" {
-  description = "List of description values across all api_management_email_templates"
-  value       = [for k, v in azurerm_api_management_email_template.api_management_email_templates : v.description]
+  description = "Map of description values across all api_management_email_templates, keyed the same as var.api_management_email_templates"
+  value       = { for k, v in azurerm_api_management_email_template.api_management_email_templates : k => v.description }
 }
 output "api_management_email_templates_resource_group_name" {
-  description = "List of resource_group_name values across all api_management_email_templates"
-  value       = [for k, v in azurerm_api_management_email_template.api_management_email_templates : v.resource_group_name]
+  description = "Map of resource_group_name values across all api_management_email_templates, keyed the same as var.api_management_email_templates"
+  value       = { for k, v in azurerm_api_management_email_template.api_management_email_templates : k => v.resource_group_name }
 }
 output "api_management_email_templates_subject" {
-  description = "List of subject values across all api_management_email_templates"
-  value       = [for k, v in azurerm_api_management_email_template.api_management_email_templates : v.subject]
+  description = "Map of subject values across all api_management_email_templates, keyed the same as var.api_management_email_templates"
+  value       = { for k, v in azurerm_api_management_email_template.api_management_email_templates : k => v.subject }
 }
 output "api_management_email_templates_template_name" {
-  description = "List of template_name values across all api_management_email_templates"
-  value       = [for k, v in azurerm_api_management_email_template.api_management_email_templates : v.template_name]
+  description = "Map of template_name values across all api_management_email_templates, keyed the same as var.api_management_email_templates"
+  value       = { for k, v in azurerm_api_management_email_template.api_management_email_templates : k => v.template_name }
 }
 output "api_management_email_templates_title" {
-  description = "List of title values across all api_management_email_templates"
-  value       = [for k, v in azurerm_api_management_email_template.api_management_email_templates : v.title]
+  description = "Map of title values across all api_management_email_templates, keyed the same as var.api_management_email_templates"
+  value       = { for k, v in azurerm_api_management_email_template.api_management_email_templates : k => v.title }
 }
 
